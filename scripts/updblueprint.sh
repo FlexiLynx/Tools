9cfedce7575303656096d93929913e29dc82406c
Shae <a.thomas.vc@gmail.com>
2024-03-14 15:38:21 -0700

    Added automatic meta-version tagging

--- scripts/updblueprint.sh
+++ scripts/updblueprint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd ./FlexiLynxCore
-python3.12 ../Tools/blueutils.py add files - --root . `git ls-tree -r main --name-only $(find . -name '*.py' -not -path './Tools/**' -not -name 'test.py')`
-echo # add trailing newline
+blue=`cat`
+# add files
+blue=`echo $blue | python3.12 ../Tools/blueutils.py add files - --root . $(git ls-tree -r main --name-only $(find . -name '*.py' -not -name 'test.py'))`
+# set version tag and output
+prev=`echo $blue | python3.12 ../Tools/blueutils.py read - 'version'`
+echo $blue | python3.12 ../Tools/blueutils.py mod - 'version' "'Git commit: $(git rev-parse --short HEAD) (prev: $(echo "${prev#\'Git commit: }" | cut -d ' ' -f 1))'"
+# cleanup
+echo
 cd ..
